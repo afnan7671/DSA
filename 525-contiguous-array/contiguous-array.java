4516acfe597cd919929855e06d63229bb0c4beb0
MD ABDUL AFNAN <abdulafnan7671@gmail.com>
2026-08-20 23:05:21 +0530

    Time: 23 ms (85.29%) | Memory: 65.5 MB (59.84%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int count = 0;
+        int max = 0;
+
+        map.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0)
+                count--;
+            else
+                count++;
+
+            if (map.containsKey(count)) {
+                max = Math.max(max, i - map.get(count));
+            }
+            else {
+                map.put(count, i);
+            }
+        }
+
+        return max;
+    }
+}
