04d84beddcb162e7ebbc696c32712a8c811ab982
MD ABDUL AFNAN <abdulafnan7671@gmail.com>
2026-08-21 17:28:13 +0530

    Time: 23 ms (85.32%) | Memory: 65.7 MB (13.78%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -3,28 +3,22 @@ import java.util.*;
 class Solution {
     public int findMaxLength(int[] nums) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+       HashMap<Integer,Integer> set=new HashMap<>();
+       int max=0;
+       int sum=0;
+       set.put(0,-1);
+       for(int i=0;i<nums.length;i++)
+       {
+        if(nums[i]==0)
+        sum++;
+        else
+        sum--;
+       if(set.containsKey(sum))
+        max=Math.max(max,i-set.get(sum));
+       else
+       set.put(sum,i);
+       }
+       return max;
 
-        int count = 0;
-        int max = 0;
-
-        map.put(0, -1);
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (nums[i] == 0)
-                count--;
-            else
-                count++;
-
-            if (map.containsKey(count)) {
-                max = Math.max(max, i - map.get(count));
-            }
-            else {
-                map.put(count, i);
-            }
-        }
-
-        return max;
     }
 }
