dcd0682ca1a7d83cf30680116e659fa756b9abcb
MD ABDUL AFNAN <abdulafnan7671@gmail.com>
2026-08-27 05:14:29 +0530

    Time: 1 ms (98.05%) | Memory: 42.5 MB (63.18%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,17 +1,6 @@
 class Solution {
     public int addDigits(int num) {
-        if(num>=0&&num<=9)
-        return num;
-   
-        while(num>9){
-        int sum=0;
-        while(num>0)
-        {
-            sum+=num%10;
-            num=num/10;
-        }
-        num=sum;
-    }
-    return num;
+       if(num==0)return 0;
+       return (num%9==0?9:num%9);
     }
 }
